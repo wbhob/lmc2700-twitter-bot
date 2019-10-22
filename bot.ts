580a08e45e697c4dd017d9c7ba7a978d03fe3734
Wilson Hobbs <wilsonhobbs1@gmail.com>
2019-10-22 17:10:05 -0400

    fix broken types

--- bot.ts
+++ bot.ts
@@ -10,36 +10,28 @@ var T = new Twit(config)
 var mediaArtsSearch: Twit.Params = {
   q: '#mediaarts',
   count: 10,
-  result_type: 'recent'
+  result_type: 'recent',
 }
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-  T.get('search/tweets', mediaArtsSearch, function(error, data: Twit.Response) {
-    // log out any errors and responses
-    console.log(error, data)
+async function retweetLatest() {
+  try {
+    const data: any = await T.get('search/tweets', mediaArtsSearch)
+
     // If our search request to the server had no errors...
-    if (!error) {
-      // ...then we grab the ID of the tweet we want to retweet...
-      var retweetId = data.statuses[0].id_str
-      // ...and then we tell Twitter we want to retweet it!
-      T.post('statuses/retweet/' + retweetId, {}, function(error, response) {
-        if (response) {
-          console.log(
-            'Success! Check your bot, it should have retweeted something.'
-          )
-        }
-        // If there was an error with our Twitter call, we print it out here.
-        if (error) {
-          console.log('There was an error with Twitter:', error)
-        }
-      })
-    }
-    // However, if our original search request had an error, we want to print it out here.
-    else {
-      console.log('There was an error with your hashtag search:', error)
-    }
-  })
+
+    // ...then we grab the ID of the tweet we want to retweet...
+    var retweetId = data.statuses[0].id_str
+    // ...and then we tell Twitter we want to retweet it!
+    const response = await T.post('statuses/retweet/' + retweetId, {})
+
+    if (response)
+      console.log(
+        'Success! Check your bot, it should have retweeted something.'
+      )
+  } catch (err) {
+    console.log('there was an error: ' + err)
+  }
 }
 
 // Try to retweet something as soon as we run the program...
--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
+
 export const config = {
-  consumer_key: 'blah',
-  consumer_secret: 'blah',
-  access_token: 'blah',
-  access_token_secret: 'blah'
+  consumer_key: process.env.CONSUMER_KEY || 'blah',
+  consumer_secret: process.env.CONSUMER_SECRET || 'blah',
+  access_token: process.env.ACCESS_TOKEN || 'blah',
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET || 'blah',
 }
